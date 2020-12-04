44ca0a89f66f6633262010465b56d932f180b3be
Kafil Nasdami <kafil.nasdami@gmail.com>
2020-12-04 23:23:20 +0100

    🎨 Updating the favicon

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,32 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div>
       <Head>
         <title>Kafil Nasdami</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
-      <div className=''>
+      <div className="">
         <h1>Welcome to my Blog</h1>
       </div>
     </div>
-  )
+  );
 }
