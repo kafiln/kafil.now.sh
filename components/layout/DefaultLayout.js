d28d9c43a1400a51a4ac22ccc8fb750f3f07ac0b
Kafil Nasdami <kafil.nasdami@gmail.com>
2020-12-05 12:36:27 +0100

    Fix: meta tag charset attribut

--- components/layout/DefaultLayout.js
+++ components/layout/DefaultLayout.js
@@ -8,12 +8,12 @@ const DefaultLayout = ({ children }) => {
       {/* TODO: Refactor into a SEO Component */}
       <Head>
         <title>Kafil Nasdami</title>
-        <meta charset="UTF-8" />
+        <meta charSet="UTF-8" />
         <meta
           name="description"
           content="The personal website of Kafil Nasdami"
         />
-        <meta name="keywords" content="Blog,Portfolio" />
+        <meta name="keywords" content="Blog, Portfolio, Pesonal Website, Full stack developer" />
         <meta name="author" content="Kafil Nasdami" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link
