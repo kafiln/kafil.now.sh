050c29a7fbcd9fc348b56216aea7eb57154b4999
Kafil Nasdami <kafil.nasdami@gmail.com>
2020-12-05 02:16:52 +0100

    Adding nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,12 @@
 import "../styles/globals.css";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+import Router from "next/router";
+
+//Binding events.
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
