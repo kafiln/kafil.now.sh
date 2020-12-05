de7fde531c85eeefa50acc8522f7099beb944f96
Kafil Nasdami <kafil.nasdami@gmail.com>
2020-12-05 17:13:17 +0100

    Adding i18n in routes

---
+++ next.config.js
@@ -0,0 +1,8 @@
+// next.config.js
+module.exports = {
+  i18n: {
+    localeDetection: false,
+    locales: ["en", "fr", "ar"],
+    defaultLocale: "en",
+  },
+};
--- pages/blog.js
+++ pages/blog.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { Layout } from "../components/layout";
+import { useRouter } from "next/router";
 
 const Blog = () => {
-  return <Layout>The Blog page</Layout>;
+  const { locale } = useRouter();
+  return <Layout>The Blog page for the language {locale}</Layout>;
 };
 
 export default Blog;
