9a150b20736e446c9c4b74eadc2b60421d9e91db
Kafil Nasdami <kafil.nasdami@gmail.com>
2021-01-08 20:58:50 +0100

    Fix routes issues for Google Analytics #14

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
-import Router from 'next/router';
+import Router, { useRouter } from 'next/router';
 import NProgress from 'nprogress';
 import 'nprogress/nprogress.css';
 import { useEffect } from 'react';
@@ -12,18 +12,18 @@ Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
   const isProduction = process.env.NODE_ENV === 'production';
 
   useEffect(() => {
     const handleRouteChange = (url: URL) => {
-      /* invoke analytics function only for production */
       if (isProduction) gtag.pageview(url);
     };
-    Router.events.on('routeChangeComplete', handleRouteChange);
+    router.events.on('routeChangeComplete', handleRouteChange);
     return () => {
-      Router.events.off('routeChangeComplete', handleRouteChange);
+      router.events.off('routeChangeComplete', handleRouteChange);
     };
-  }, [Router.events]);
+  }, [router.events]);
 
   return (
     <ChakraProvider>
