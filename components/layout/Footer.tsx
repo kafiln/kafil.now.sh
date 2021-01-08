d890e5b1d3b9e5b9852ac386dbb3a9b001fca8f4
Kafil Nasdami <kafil.nasdami@gmail.com>
2021-01-08 03:14:29 +0100

    Using different colors for header and footer #19

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,40 +1,55 @@
-import { Flex, Heading, Box, HStack } from "@chakra-ui/react";
-import React from "react";
-import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+import {
+  Flex,
+  Heading,
+  Box,
+  HStack,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import React from 'react';
+import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
 const Footer = () => {
+  const bg = useColorModeValue('red.500', 'red.100');
+  const color = useColorModeValue('white', 'blue.900');
+
   const socials = [
     {
       icon: FaGithub,
       display: true,
-      name: "github",
-      url: "https://github.com/kafiln",
+      name: 'github',
+      url: 'https://github.com/kafiln',
     },
     {
       icon: FaTwitter,
       display: true,
-      name: "twitter",
-      url: "https://twitter.com/kafiiil",
+      name: 'twitter',
+      url: 'https://twitter.com/kafiiil',
     },
     {
       icon: FaLinkedin,
       display: true,
-      name: "linkedin",
-      url: "https://www.linkedin.com/in/kafiln/",
+      name: 'linkedin',
+      url: 'https://www.linkedin.com/in/kafiln/',
     },
-    { name: "blog", url: "https://kafil.now.sh" },
-    { name: "mail", url: "mailto:kafil.nasdami@gmail.com" },
+    { name: 'blog', url: 'https://kafil.now.sh' },
+    { name: 'mail', url: 'mailto:kafil.nasdami@gmail.com' },
   ];
   return (
-    <Flex p={4} as="footer" justifyContent="space-between">
+    <Flex
+      p={4}
+      as="footer"
+      justifyContent="space-between"
+      color={color}
+      bg={bg}
+    >
       <Heading as="h1" mx={2} size="sm">
         Kafil NASDAMI
       </Heading>
       <HStack as="ul" spacing={10}>
         {socials
-          .filter((item) => item.display)
-          .map((item) => {
+          .filter(item => item.display)
+          .map(item => {
             return (
-              <Box as="li" key={item.name} style={{ listStyle: "none" }}>
+              <Box as="li" key={item.name} style={{ listStyle: 'none' }}>
                 <a
                   aria-label={item.name}
                   href={item.url}
--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,4 +1,4 @@
-import { Flex, HStack } from '@chakra-ui/react';
+import { Flex, HStack, useColorModeValue } from '@chakra-ui/react';
 import React from 'react';
 import { Brand, DesktopMenu, MobileMenu } from '.';
 import { ThemeToggle, HomeButton } from '../common';
@@ -11,8 +11,17 @@ const Header = () => {
     { name: 'contact' },
   ];
 
+  const bg = useColorModeValue('red.500', 'red.100');
+  const color = useColorModeValue('white', 'blue.900');
+
   return (
-    <Flex as="header" p={4} justifyContent="space-between">
+    <Flex
+      as="header"
+      bg={bg}
+      color={color}
+      p={4}
+      justifyContent="space-between"
+    >
       <Brand />
       <HStack as="nav" alignItems="center" justifyContent="center" spacing={2}>
         <DesktopMenu pages={pages} />
