bca4e95306ec5fc3add56db5de41f0b3d0fdc07c
Kafil Nasdami <kafil.nasdami@gmail.com>
2021-10-23 00:36:33 +0100

    tests: add tests for toggleTheme component

--- components/common/ThemeToggle/ThemeToggle.test.tsx
+++ components/common/ThemeToggle/ThemeToggle.test.tsx
@@ -1,9 +1,38 @@
-import { render } from '@testing-library/react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { fireEvent, render } from '@testing-library/react';
 import ThemeToggle from '.';
 
 describe('ThemeToggle', () => {
   it('should render without crashing', () => {
     const { asFragment } = render(<ThemeToggle />);
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('should have a default size of 1.5em', () => {
+    const { container } = render(<ThemeToggle />);
+    const height = container.querySelector('svg').getAttribute('height');
+    expect(height).toEqual('1.5em');
+  });
+
+  it('should have a size from prop', () => {
+    const { container } = render(<ThemeToggle size={4} />);
+    const height = container.querySelector('svg').getAttribute('height');
+    expect(height).toEqual('4em');
+  });
+
+  it('should change icon on click', () => {
+    const { container } = render(
+      <ChakraProvider>
+        <ThemeToggle />
+      </ChakraProvider>
+    );
+    const box = container.firstChild;
+    const sun = container.querySelector('svg');
+
+    fireEvent.click(box);
+    const moon = container.querySelector('svg');
+    expect(sun).toMatchSnapshot();
+    expect(moon).toMatchSnapshot();
+    expect(sun).not.toEqual(moon);
   });
 });
--- components/common/ThemeToggle/index.tsx
+++ components/common/ThemeToggle/index.tsx
@@ -1,13 +1,17 @@
-import { useColorMode, Box } from '@chakra-ui/react';
+import { Box, useColorMode, useColorModeValue } from '@chakra-ui/react';
 import React from 'react';
 import { FaMoon, FaSun } from 'react-icons/fa';
 
-const ThemeToggle = () => {
-  const { colorMode, toggleColorMode } = useColorMode();
-  const Component = colorMode === 'dark' ? FaSun : FaMoon;
+export type ThemeToggleProps = {
+  size?: number;
+};
+
+const ThemeToggle = ({ size = 1.5 }: ThemeToggleProps) => {
+  const { toggleColorMode } = useColorMode();
+  const Component = useColorModeValue(FaSun, FaMoon);
   return (
     <Box onClick={toggleColorMode} style={{ cursor: 'pointer' }}>
-      <Component size="1.5em" />
+      <Component size={`${size}em`} />
     </Box>
   );
 };
